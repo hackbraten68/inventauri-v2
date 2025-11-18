de84180db6a87b463d4911deb3466091645d14c6
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2025-11-18 19:11:01 +0100

    add: Amplify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: awsAmplify(),
   integrations: [
     react(),
     tailwind({
