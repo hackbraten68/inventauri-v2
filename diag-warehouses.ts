9c3b0125e727f5af90b1bf08fe0b0aa034796e54
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2026-01-27 20:35:36 +0100

    remove temp files

--- diag-warehouses.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-    const warehouses = await prisma.warehouse.findMany();
-    console.log(JSON.stringify(warehouses, null, 2));
-}
-
-main().finally(() => prisma.$disconnect());
